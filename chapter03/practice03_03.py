f1d03f5652d84846c6854a89430acc59559fb75a
Aemilus <35335174+Aemilus@users.noreply.github.com>
2022-06-05 18:21:13 +0300

    practice03_03

---
+++ chapter03/practice03_03.py
@@ -0,0 +1,7 @@
+def print_name(name):
+    print(f"My name is {name}.")
+
+
+if __name__ == '__main__':
+    return_value = print_name('Gerard')
+    print(f"Return value of previous function call is: {return_value}")
