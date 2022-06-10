5c5b0805ddfcab8ed11240c48e2a9bc640af2867
Aemilus <35335174+Aemilus@users.noreply.github.com>
2022-06-10 12:36:23 +0300

    exercise04_05

---
+++ chapter04/practice04_05.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    colors = ['red', 'black', 'blue', 'white', 'orange', 'yellow', 'green']
+    print(colors)
+
+    # length of entire list
+    colors_len = len(colors)
+    print('Length:', colors_len)
+
+    # length of a sliced list
+    my_colors = colors[4:]
+    print(my_colors)
+    print(len(my_colors))
